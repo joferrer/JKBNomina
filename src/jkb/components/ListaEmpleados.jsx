cba4784a21eb036b3a16dec8a6efdbdac1e3b21a
Jeison Ferrer <jeison120120@hotmail.com>
2022-09-29 18:27:01 -0500

    Lista de empleados y boton de agregar sin implementar

--- src/jkb/components/ListaEmpleados.jsx
+++ src/jkb/components/ListaEmpleados.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Divider, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material"
+import { Avatar, Box, Divider, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material"
 import { EmpleadoInfo } from "./EmpleadoInfo"
 
 
@@ -48,24 +48,25 @@ export const ListaEmpleados = () => {
 
     }]
   return (
-    <List sx={{ width: '100%', height: '100%', bgcolor: 'background.paper' }}>
+    <List sx={{ width: '100%', height: '100%', bgcolor: 'background.paper' ,
+    display:'flex', flexDirection:'column',alignContent: 'center'}}>
         {listaEmpleados.map((empleado, index)=>(
             <>
-                <ListItem alignItems="flex-start">
+                <ListItem  alignItems="center" key={`empleado ${index}`}>
                     <ListItemAvatar>
-                        <Avatar  sx={{ width: 100, height: 100, marginRight: 1}}>J</Avatar>
+                        <Avatar  sx={{ width: 90, height: 90, marginRight: 1}}>J</Avatar>
                     </ListItemAvatar>
-                    <ListItemText primary={<Typography variant="h5">{empleado.nombre}</Typography>}
-                        secondary={<>
-                            <Typography variant="subtitle1">{empleado.correo}</Typography>
-                            <Typography variant="subtitle1">{empleado.edad}</Typography>
-                            <Typography variant="subtitle1">{empleado.rol}</Typography>
-                        </>}
-                    />
-                     
-           
+                    <Box>
+                    <Typography variant="h5">{empleado.nombre}</Typography>
+                        <List>
+                            <ListItem key={`correo ${index}`}>{empleado.correo}</ListItem>
+                            <ListItem key={`edad ${index}`}>{empleado.edad}</ListItem>
+                            <ListItem key={`rol ${index}`}>{empleado.rol}</ListItem>
+                         </List>
+                    </Box>
+                    
                 </ListItem>
-                <Divider variant="inset" component="li" />
+                <Divider variant="inset" component="li"  key={`divider ${index}`} />
             </>
             
             )
--- src/jkb/pages/Empleados.jsx
+++ src/jkb/pages/Empleados.jsx
@@ -1,4 +1,5 @@
-import { Typography } from "@mui/material"
+import { Button, Typography } from "@mui/material"
+import { Box } from "@mui/material"
 import { ListaEmpleados } from "../components/ListaEmpleados"
 import { JKBNominaLayout } from "../layout/JKBNominaLayout"
 import { NothingSelectedView } from "../views/NothingSelectedView"
@@ -8,8 +9,12 @@ export const Empleados = () => {
     <>
       <JKBNominaLayout >
         {/*<NothingSelectedView/>*/}
+        <Box sx={{display:'flex' , justifyContent:'space-between' , alignContent:'center'}}>
+          <Typography variant="h3" >Empleados</Typography>
+          <Button variant="contained">Agregar empleado</Button>
 
-        <Typography variant="h3" >Empleados</Typography>
+        </Box>
+        
         <ListaEmpleados/>
       </JKBNominaLayout>
     </>
